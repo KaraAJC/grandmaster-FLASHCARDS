a3a3a331d0d023e66b5f11a059b47f4611553fa8
Hola! Me llamo Casey <jordanedwardobey@gmail.com>
2015-02-27 17:43:29 -0600

    added rounds table

--- db/migrate/20150227170206_round.rb
+++ db/migrate/20150227170206_round.rb
@@ -1,6 +1,6 @@
 class Round < ActiveRecord::Migration
   def change
-    create_table do |t|
+    create_table :rounds do |t|
       t.belongs_to :deck
       t.belongs_to :player
       t.boolean :number_correct
