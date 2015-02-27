773d903a996af8e70937e727f53bf1560f80d93b
Stephanie Marx <stephanie.n.marx@gmail.com>
2015-02-27 17:29:10 -0600

    Add seed for decks and cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,21 @@
 Deck.create(name: 'Spanish Colors')
 
 
-cards = [{question: thing1, answer: thing2}, ]
+span_colors = [{question: negro, answer: black}, 
+							 {question: blanco, answer: white},
+							 {question: gris, answer: gray},
+							 {question: rojo, answer: red},
+							 {question: azul, answer: blue},
+							 {question: amarillo, answer: yellow},
+							 {question: verde, answer: green},
+							 {question: naranja, answer: orange},
+							 {question: morado, answer: purple},
+							 {question: rosa, answer: pink},
+							 {question: marrón, answer: brown},
+							 {question: plateado, answer: silver},
+							 {question: dorado, answer: gold}
+]
 
-cards.each do |card|
+span_colors.each do |card|
 	Card.new(deck_id: Deck.find_by(name: 'Spanish Colors').id, question: card[name], answer: card[answer])
 end
