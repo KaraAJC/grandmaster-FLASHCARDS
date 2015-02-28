d48bfe08c7cb54a1992b399bbf7f8a80f9e4e489
Hola! Me llamo Casey <jordanedwardobey@gmail.com>
2015-02-28 14:02:44 -0600

    made a working round! WOO WOO!

--- app/controllers/deck.rb
+++ app/controllers/deck.rb
@@ -1,7 +1,19 @@
 get '/decks' do
+  puts params
+  session.each do |k,v|
+    puts "key is #{k}, value is #{v}"
+  end
   @decks = Deck.all
 
   erb :"deck/all"
+end
+
+get '/decks/:deck_id' do
+  puts params
+  @deck = Deck.find(params[:deck_id])
+  @user = User.find(session[:user_id])
+  @round = Round.create( deck_id: @deck.id, player_id: @user.id )
+  redirect "/decks/#{@deck.id}/cards/#{@deck.cards.first.id}"
 end
 
 get '/decks/:deck_id/cards/:card_id' do
@@ -11,4 +23,8 @@ get '/decks/:deck_id/cards/:card_id' do
   erb :"card/show"
 end
 
+# post 'decks/:deck_id/cards/:card_id' do
+#   @
 
+#   erb: "card/answer"
+# end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,7 +1,12 @@
 class Card < ActiveRecord::Base
   belongs_to :deck
 
-  validates :question, presence: true 
-  validates :answer, presence: true 
-  validates :deck_id, presence: true 
+  validates :question, presence: true
+  validates :answer, presence: true
+  validates :deck_id, presence: true
+
+  def check(response)
+    Return true if response == self.answer
+  end
+
 end
